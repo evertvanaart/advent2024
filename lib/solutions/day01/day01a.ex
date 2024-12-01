bb8b16e17dd90d0b9d5001c17be7d0bb5d500269
Evert van Aart <evert.vanaart@pm.me>
2024-12-01 18:27:01 +0100

    Day 1

--- lib/solutions/day01/day01a.ex
+++ lib/solutions/day01/day01a.ex
@@ -1,11 +1,11 @@
 import Solutions.Day01.Common
 
 # An easy first day, and the kind of problem that can be solved very elegantly in
-# Elixir. We first parse the lines into two separate lists – Enum.unzip() is very
+# Elixir. We first parse the lines into two separate lists – Enum.unzip/1 is very
 # useful here – then sort them and zip them back up, this time computing the sum
 # of the absolute difference of each pair. At around six milliseconds it isn't
 # particularly fast, but based on some superficial profiling most of this time
-# comes from the String.split() call, which we can't easily avoid.
+# comes from the String.split/1 call, which we can't easily avoid.
 
 defmodule Solutions.Day01.PartA do
   def solve(lines) do
--- lib/solutions/day01/day01b.ex
+++ lib/solutions/day01/day01b.ex
@@ -2,9 +2,13 @@ import Solutions.Day01.Common
 
 # After parsing the two lists like we did in the first part, we can easily obtain a
 # frequencies map – i.e., values to how often a value appears in a list – using the
-# Enum.frequencies() function. We then simply iterate over the pairs of the left-hand
-# frequency map, and multiply the left-hand frequency by the right-hand frequency (or
-# zero) and by the actual value. Taking the sum of these products gives the answer.
+# Enum.frequencies/1 function. We know that only values that appear in both lists
+# contribute to the final sum, which corresponds to the intersection of these two
+# frequency maps. Elixir's Map.intersect/3 allows us to pass a function for resolving
+# conflicts, and we use this function to multiply the frequencies in the left and right
+# lists. Then, all we need to do is iterate over all key-value pairs in the intersected
+# map, compute the products of the key and the value, and sum up these products; we can
+# use Enum.reduce/3 to do both operations in a single pass.
 
 defmodule Solutions.Day01.PartB do
   def solve(lines) do
@@ -13,6 +17,7 @@ defmodule Solutions.Day01.PartB do
     lfreqs = Enum.frequencies(left)
     rfreqs = Enum.frequencies(right)
 
-    lfreqs |> Stream.map(fn {v, f} -> v * f * Map.get(rfreqs, v, 0) end) |> Enum.sum()
+    Map.intersect(lfreqs, rfreqs, fn _, l, r -> l * r end)
+    |> Enum.reduce(0, fn {k, v}, acc -> acc + k * v end)
   end
 end
